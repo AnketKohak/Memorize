b55d4d48d33c955dcc6b6a2523ed6a2ff4a92422
AnketKohak <anketkohak40@gmail.com>
2024-12-27 22:14:35 +0530

    adding button functionality to add or remove card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,24 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis : Array<String> = ["👻","🎃","🕷️"," 😈","💀","🕸️","🧙‍♀️","🙀","👹","😱","☠️","🍭"]
+    @State var cardCount:Int = 4
     var body: some View {
+        VStack {
+            card
+            cardAdjusters
+        }.padding()
+    }
+    
+    var card: some View{
         HStack{
-            let emojis : Array<String> = ["👻","🎃","🕷️"," 😈"]
-            ForEach(emojis.indices,id: \.self){ index in
+            ForEach(0..<cardCount,id: \.self){ index in
                 CardView(content:  emojis[index])
             }
         }
         .foregroundStyle(.orange)
-        .padding()
+    }
+    
+    var cardAdjusters: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+        .foregroundStyle(.blue)
+    }
+    
+    var cardRemover: some View{
+        Button(action: {
+            if cardCount > 1{
+                cardCount -= 1
+            }
+        }, label :{
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+        })
+    }
+    
+    var cardAdder: some View{
+        Button(action :{
+            if cardCount < emojis.count{
+                cardCount += 1
+            }
+        }, label :{
+            Image(systemName: "rectangle.stack.badge.plus.fill")
+        })
     }
 }
+
+
 struct CardView : View{
     let content: String
     @State var isFaceUp : Bool = true
+    
     var body : some View{
         ZStack{
             let base =  RoundedRectangle(cornerRadius: 12)
-
             if isFaceUp{
                base.foregroundStyle(.white)
                base.strokeBorder(lineWidth: 2)
@@ -33,7 +73,6 @@ struct CardView : View{
             }
             else{
                base.fill()
-
             }
         }.onTapGesture() {
             isFaceUp.toggle()
