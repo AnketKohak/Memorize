b2ecd6a8dc91ca2f446a2d51b6699d5d8bfe8115
AnketKohak <anketkohak40@gmail.com>
2024-12-27 12:35:18 +0530

    First lecture is Done

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
-            CardView()
+            CardView(isFaceUp: true)
             CardView()
             CardView()
 
@@ -19,15 +19,22 @@ struct ContentView: View {
         .padding()
     }
 }
-
 struct CardView : View{
+    var isFaceUp : Bool = false
     var body : some View{
         ZStack{
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundStyle(.white)
-            RoundedRectangle(cornerRadius: 12)
-                .strokeBorder(lineWidth: 2)
-            Text("👻")
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻")
+                    .font(.largeTitle)
+            }
+            else{
+                RoundedRectangle(cornerRadius: 12)
+
+            }
         }
     }
 }
