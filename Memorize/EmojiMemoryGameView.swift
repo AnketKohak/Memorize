b6a2ef585e506876839ce2fa501e19f03e05f4ef
Anket Kohak <anketkohak40@gmail.com>
2025-01-12 14:08:23 +0530

    GeometryReader added to flexible ui

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 
 struct EmojiMemoryGameView: View {
     @ObservedObject   var viewModel : EmojiMemoryGame
+    private let aspectRatio : CGFloat = 2/3
     var body: some View {
         VStack {
-            ScrollView{
-                card
-                    .animation(.default, value: viewModel.cards)
-            }
+            card
+                .animation(.default, value: viewModel.cards)
+            
             Button("Suffule"){
                 viewModel.shuffle()
             }
@@ -23,17 +23,43 @@ struct EmojiMemoryGameView: View {
     }
     
     var card: some View{
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85 ), spacing: 0)]){
-            ForEach(viewModel.cards){ card in
-                CardView(card)
-                    .aspectRatio(2/3,contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
+       
+        GeometryReader{  geometry in
+            let gridItemSize = gridItemWidthThatFits(
+                count: viewModel.cards.count,
+                size: geometry.size,
+                atAspectRatio: aspectRatio            )
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize ), spacing: 0)]){
+                ForEach(viewModel.cards){ card in
+                    CardView(card)
+                        .aspectRatio(aspectRatio,contentMode: .fit)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                }
             }
         }
         .foregroundStyle(.orange)
+    }
+    func gridItemWidthThatFits(
+        count : Int,
+        size : CGSize,
+        atAspectRatio aspectRatio : CGFloat
+    )-> CGFloat{
+        let count = CGFloat(count)
+        var columnCount = 1.0
+        repeat{
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+            
+            let rowCount = (count / columnCount).rounded(.up)
+            if rowCount * height < size.height{
+                return (size.width / columnCount).rounded(.down)
+            }
+            columnCount += 1
+        }while columnCount < count
+        return min(size.width / count, size.height * aspectRatio).rounded(.down)
     }
 }
 // Cardview
@@ -46,8 +72,8 @@ struct CardView : View{
         ZStack{
             let base = RoundedRectangle(cornerRadius: 12)
             Group{
-               base.foregroundStyle(.white)
-               base.strokeBorder(lineWidth: 2)
+                base.foregroundStyle(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text(card.content)
                     .font(.system(size: 200))
                     .minimumScaleFactor(0.01)
